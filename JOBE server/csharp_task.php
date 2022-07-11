3b1a0c97f5aad275bb7e590c40d21cd890ea2af1
Richard Pawson <rpawson@nakedobjects.net>
2022-07-11 10:29:53 +0100

    Folder structure

---
+++ JOBE server/csharp_task.php
@@ -0,0 +1,52 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* ==============================================================
+ *
+ * C Sharp
+ *
+ * ==============================================================
+ *
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('application/libraries/LanguageTask.php');
+
+class CSharp_Task extends Task {
+
+    public function __construct($filename, $input, $params) {
+        parent::__construct($filename, $input, $params);
+        $this->default_params['memorylimit'] = 600; 
+
+        $this->default_params['compileargs'] = array();
+    }
+
+    public static function getVersionCommand() {
+        return array('mono --version', '/version ([0-9.]+)/');
+    }
+
+    public function compile() {
+        $src = basename($this->sourceFileName);
+        $this->executableFileName = $execFileName = "$src.exe";
+        $compileargs = $this->getParam('compileargs');
+        $cmd = "mcs " . implode(' ', $compileargs) . "$src -out:$src.exe 2>&1";
+        list($output, $this->cmpinfo) = $this->run_in_sandbox($cmd);
+        if(preg_match('/.*error.*/', $output, $matches)) {
+                $this->cmpinfo = $matches[0];
+
+        }
+    }
+
+    // A default name for C programs
+    public function defaultFileName($sourcecode) {
+        return 'prog.cs';
+    }
+     // The executable is the output from the compilation
+    public function getExecutablePath() {
+        return "/usr/bin/mono";
+    }
+
+
+    public function getTargetFile() {
+        return $this->sourceFileName . ".exe";
+    }
+};
