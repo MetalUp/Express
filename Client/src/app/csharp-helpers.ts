8231752b3b78e04d0966afb9378fc6bec95b62e7
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-07-19 11:58:42 +0100

    #1 add csharp formatting code

--- Client/src/app/csharp-helpers.ts
+++ Client/src/app/csharp-helpers.ts
@@ -4,10 +4,35 @@ export function wrapCSharpExpression(expression : string) {
     return `
     using System;
     using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
     
     class Hello {
+
+        private static string Display(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            if (obj is string)
+            {
+                return $@"""{obj}""";
+            }
+
+            if (obj is IEnumerable)
+            {
+                var display = ((IEnumerable)obj).Cast<object>().Select(o => Display(o));
+                return $@"{{{string.Join(',', display)}}}";
+            }
+
+            return obj.ToString();
+        }
+
+
         static void Main(string[] args) {
-           System.Console.WriteLine(${expression});
+           System.Console.WriteLine(Display(${(expression)}));
        }
     }`;
 }
