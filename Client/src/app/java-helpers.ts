bcc39f8e233e1ee6648c36b6174c6a2b4dad1df6
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-07-19 13:29:55 +0100

    #1 add java formatting code

--- Client/src/app/java-helpers.ts
+++ Client/src/app/java-helpers.ts
@@ -3,10 +3,34 @@ import { defaultRegExp, filterCmpinfoWithRegex, findFunctionsWithRegex, validate
 export function wrapJavaExpression(expression : string) {
     return `
     import java.util.*;
+    import java.lang.*;
     
     class prog {
+
+        private static String display(Object obj) {
+            if ((obj == null)) {
+                return null;
+            }
+            
+            if ((obj instanceof String)) {
+                return '"' + obj.toString() + '"';
+            }
+            
+            if ((obj instanceof Iterable)) {
+                List<String> display = new ArrayList<String>();
+
+                for(Object elem : (Iterable)obj){
+                    display.add(display(elem));
+                }
+
+                return "{" +  String.join(",", display) + "}";
+            }
+            
+            return obj.toString();
+        }
+
         public static void main(String[] args) {
-            System.out.println(${expression}); 
+            System.out.println(display(${expression})); 
         }
     }`;
 }
