bfbffe2171b19554a1c234ec50ee1228d7f9dfe5
Richard Pawson <rpawson@nakedobjects.net>
2022-07-27 15:30:01 +0100

    #2 updating of rules

--- Client/src/app/languages/python-helpers.ts
+++ Client/src/app/languages/python-helpers.ts
@@ -1,8 +1,10 @@
+import { DATE_PIPE_DEFAULT_TIMEZONE } from "@angular/common";
 import { FunctionPlaceholder } from "./language-helpers";
 
 export function wrapPythonExpression(expression : string) {
     return `
 from math import *
+from datetime import date, datetime
 
 ${FunctionPlaceholder}
 
