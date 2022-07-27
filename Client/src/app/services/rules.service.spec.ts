57650ddc2c6b08efb1eaa4f5094cc3a6f48c58d7
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-07-27 12:18:54 +0100

    #2 tweak test

--- Client/src/app/services/rules.service.spec.ts
+++ Client/src/app/services/rules.service.spec.ts
@@ -7,6 +7,11 @@ describe('RulesService', () => {
   let service: RulesService;
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
  
+  enum language{
+    csharp = 'csharp',
+    python = 'python'
+  }
+
 
   beforeEach(() => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
@@ -20,33 +25,33 @@ describe('RulesService', () => {
   });
 
   it('should filter csharp cmpinfo', () => {
-    const filtered = service.filter('csharp', ErrorType.cmpinfo, "something CSXXXX something else");
+    const filtered = service.filter(language.csharp, ErrorType.cmpinfo, "something CSXXXX something else");
     expect(filtered).toEqual('CSXXXX something else')
   });
  
   it('should filter csharp stderr', () => {
-    const filtered = service.filter('csharp', ErrorType.stderr, "something NullException something else");
+    const filtered = service.filter(language.csharp, ErrorType.stderr, "something NullException something else");
     expect(filtered).toEqual('NullException')
   });
 
   it('should filter python cmpinfo', () => {
-    const filtered = service.filter('python', ErrorType.cmpinfo, "something Error: something else");
+    const filtered = service.filter(language.python, ErrorType.cmpinfo, "something Error: something else");
     expect(filtered).toEqual(' Error: something else')
   });
  
   it('should filter python stderr', () => {
-    const filtered = service.filter('python', ErrorType.stderr, "something Error: something else");
+    const filtered = service.filter(language.python, ErrorType.stderr, "something Error: something else");
     expect(filtered).toEqual(' Error: something else')
   });
 
 
   it('should validate csharp expression - System', () => {
-    const validated = service.validate('csharp', Applicability.expressions, "System.");
+    const validated = service.validate(language.csharp, Applicability.expressions, "System.");
     expect(validated).toEqual("Explicit use of the 'System' namespace is unnecessary, and not permitted");
   });
 
   it('should validate csharp expression - Console', () => {
-    const validated = service.validate('csharp', Applicability.expressions, "Console.");
+    const validated = service.validate(language.csharp, Applicability.expressions, "Console.");
     expect(validated).toEqual("Use of  Console. is not permitted in MetalUp.Express");
   });
 
