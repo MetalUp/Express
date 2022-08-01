ae25bfbd4c44a59a2bdb671464d67bf3a296b40d
Richard Pawson <rpawson@nakedobjects.net>
2022-08-01 17:00:11 +0100

    Tweaks to Display implementation (both Python and C#)

--- Client/src/app/languages/python-helpers.ts
+++ Client/src/app/languages/python-helpers.ts
@@ -9,12 +9,12 @@ ${FunctionPlaceholder}
 
 def display(x):
   if isinstance(x, str):
-    return x
+    return str(x)
   else:
     try:
          return list(iter(x))
     except TypeError:
-         return x
+         return str(x)
 
 print (display(${expression}))
 `;
