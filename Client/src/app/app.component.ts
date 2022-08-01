3ca458c7705a9e1c89830e7cb38d525d1bc2dc1a
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-01 18:43:21 +0100

    #5 add language class

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ile-client';
+
+  constructor(private route: ActivatedRoute) { }
+
+  language: string = '';
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.language = params['language'];
+    });
+  }
 }
