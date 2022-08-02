e038acc1c106d0edadada6e94a42ce04078b3812
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-02 10:22:20 +0100

    #5 tidy some naming

--- Client/src/app/selected-language/selected-language.component.spec.ts
+++ Client/src/app/selected-language/selected-language.component.spec.ts
@@ -2,26 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Params } from '@angular/router';
 import { of } from 'rxjs';
 import { JobeServerService } from '../services/jobe-server.service';
-
 import { SelectedLanguageComponent } from './selected-language.component';
 
 describe('SelectedLanguageComponent', () => {
   let component: SelectedLanguageComponent;
   let fixture: ComponentFixture<SelectedLanguageComponent>;
-  let mockJobeServerService: jasmine.SpyObj<JobeServerService>;
-
+  let jobeServerServiceSpy: jasmine.SpyObj<JobeServerService>;
   let testParams: any = {};
 
   beforeEach(async () => {
-    mockJobeServerService = jasmine.createSpyObj('JobeServerService', ['submit_run', 'get_languages']);
-    mockJobeServerService.get_languages.and.returnValue(of<[string, string][]>([["1", "2"]]));
+    jobeServerServiceSpy = jasmine.createSpyObj('JobeServerService', ['submit_run', 'get_languages']);
+    jobeServerServiceSpy.get_languages.and.returnValue(of<[string, string][]>([["1", "2"]]));
 
     await TestBed.configureTestingModule({
       declarations: [SelectedLanguageComponent],
       providers: [
         {
           provide: JobeServerService,
-          useValue: mockJobeServerService
+          useValue: jobeServerServiceSpy
         },
         {
           provide: ActivatedRoute,
@@ -42,7 +40,7 @@ describe('SelectedLanguageComponent', () => {
   });
 
   it('should setup selected language', () => {
-    mockJobeServerService.get_languages.and.returnValue(of<[string, string][]>([["language1", "language2"]]));
+    jobeServerServiceSpy.get_languages.and.returnValue(of<[string, string][]>([["language1", "language2"]]));
     testParams['language'] = 'language1';
 
     component.ngOnInit();
--- Client/src/app/selected-language/selected-language.component.ts
+++ Client/src/app/selected-language/selected-language.component.ts
@@ -36,5 +36,4 @@ export class SelectedLanguageComponent implements OnInit {
       }
     }
   }
-
 }
