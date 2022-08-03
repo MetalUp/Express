feeff7ec00ef973dc37560d7a1ad255283f01a90
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-03 10:51:39 +0100

    #7 update tests

--- Client/src/app/testing/testing.component.spec.ts
+++ Client/src/app/testing/testing.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ITask } from '../services/task';
+import { TaskService } from '../services/task.service';
+import { of, Subject } from 'rxjs';
 
 import { TestingComponent } from './testing.component';
+import { JobeServerService } from '../services/jobe-server.service';
 
 describe('TestingComponent', () => {
   let component: TestingComponent;
   let fixture: ComponentFixture<TestingComponent>;
+  let jobeServerServiceSpy: jasmine.SpyObj<JobeServerService>;
+  let taskServiceSpy: jasmine.SpyObj<TaskService>;
+  let taskSubject = new Subject<ITask>();
 
   beforeEach(async () => {
+    jobeServerServiceSpy = jasmine.createSpyObj('JobeServerService', ['submit_run', 'get_languages'], { "selectedLanguage": "csharp" });
+    taskServiceSpy = jasmine.createSpyObj('TaskService', ['load', 'getHtml'], { currentTask: taskSubject });
+
     await TestBed.configureTestingModule({
-      declarations: [ TestingComponent ]
-    })
-    .compileComponents();
+      declarations: [TestingComponent],
+      providers: [
+        {
+          provide: JobeServerService,
+          useValue: jobeServerServiceSpy
+        },
+        {
+          provide: TaskService,
+          useValue: taskServiceSpy
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TestingComponent);
     component = fixture.componentInstance;
