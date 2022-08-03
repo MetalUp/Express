66e7d571e5b44c47cfb20067da5cd14f889a8633
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-03 10:41:51 +0100

    #7 add python wrapping code

--- Client/src/app/languages/python-helpers.ts
+++ Client/src/app/languages/python-helpers.ts
@@ -31,5 +31,42 @@ ${functions}
 }
 
 export function wrapPythonTests(tests : string) {
-  return tests;
+  return `
+from math import *
+from datetime import date, datetime
+    
+${FunctionPlaceholder}
+
+def display(x):
+  if isinstance(x, str):
+    return str(x)
+  else:
+    try:
+         return list(iter(x))
+    except TypeError:
+         return str(x)
+
+fail = "Test failed calling "
+
+def arg_string(arguments) : return display(arguments).strip("[]")
+  
+def test_function(function_name, expected, actual, args):
+      if display(actual) != display(expected):
+          print(fail + f"{function_name}({arg_string(args)}) Expected: {display(expected)}  Actual: {display(actual)}");
+          raise TestException
+  
+def assert_true(function_name, args, actual, message) :
+      if actual is not True :
+          print(fail + f"{function_name}({arg_string(args)}) {message}")
+          raise TestException
+  
+def all_tests_passed(function_name):
+      print(f"All tests passed on function: {function_name}")
+  
+class TestException(Exception) : pass
+ 
+${tests}
+
+RunTests()
+`;
 }
