cb448e5e87dda34fbbdaabbc36f0f3de2d97d32c
Richard Pawson <rpawson@nakedobjects.net>
2022-08-03 14:22:00 +0100

    #16 Life 2 task working with hints and tests

---
+++ Client/src/content/life_2_tests.py
@@ -0,0 +1,12 @@
+def RunTests() :
+    fn = keep_within_bounds.__name__
+    def test(p1, expected):
+        test_function(fn, expected, keep_within_bounds(p1), [p1])
+    test(399, 399)
+    test(400, 0)
+    test(401, 1)
+    test(419, 19)
+    test(0, 0)
+    test(-1, 399)
+    test(-20, 380)
+    all_tests_passed()
