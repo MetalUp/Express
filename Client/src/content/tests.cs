515d2e811d125426481c40f741e1da4a3b5720fa
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-03 11:42:37 +0100

    #7 update test code

--- Client/src/content/tests.cs
+++ Client/src/content/tests.cs
@@ -1,4 +1,11 @@
 static void RunTests()
 {
-   AllTestsPassed("testing");
+   string fn = nameof(Double);
+   Action<int, int> test = (input, output) =>
+   {
+        var n = Double(input);
+        AssertTrue(fn, "", n == output, $" Expected: {output} Actual: {n}");
+   };
+   test(3, 9);
+   AllTestsPassed(fn);
 }
