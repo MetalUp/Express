57c76708ecf204ba30e5e5d663a5c187ed69bf91
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-08-09 16:04:51 +0100

    #6 fix hint index

--- Client/src/app/hint/hint.component.ts
+++ Client/src/app/hint/hint.component.ts
@@ -64,7 +64,7 @@ export class HintComponent implements OnInit, OnDestroy {
     this.sub = this.taskService.currentTask.subscribe(task => {
       this.currentTask = task;
       this.currentHint = '';
-      this.hintIndex = 0;
+      this.hintIndex = -1;
     })
   }
 
