67a64415808d37fc76f3395aaee6f9a810621114
Richard Pawson <rpawson@nakedobjects.net>
2022-08-11 18:28:19 +0100

    #16 All C# parts tested & edited.

--- Client/src/content/life_2_tests.cs
+++ Client/src/content/life_2_tests.cs
@@ -1,17 +1,20 @@
 static void RunTests()
 {
-        TestNeighbourCells(30, new List<int> { 9, 10, 11, 29, 31, 49, 50, 51 });
+    try
+    {
+        testKeepWithinBounds(0, 0);
+        testKeepWithinBounds(399, 399);
+        testKeepWithinBounds(400, 0);
+        testKeepWithinBounds(401, 1);
+        testKeepWithinBounds(419, 19);
+        testKeepWithinBounds(-1, 399);
+        testKeepWithinBounds(-20, 380);
         AllTestsPassed();
+    }
+    catch (TestFailure) { }
 }
 
-private static void TestNeighbourCells(int cell, List<int> expected)
+private static void testKeepWithinBounds(int p1, int expected)
 {
-    var n = NeighbourCells(cell);
-    string fn = nameof(NeighbourCells);
-    var msg = fail + $"{fn}({cell}).";
-    AssertTrue(fn, cell.ToString(), n.Count == 8, msg + $" Expected: 8 elements Actual: {n.Count}");
-    foreach (int val in expected)
-    {
-        AssertTrue(fn, cell.ToString(), n.Contains(val), msg + $" List does not contain: {val}");
-    }
+    TestFunction(nameof(KeepWithinBounds), expected, KeepWithinBounds(p1), p1);
 }
--- Client/src/content/life_3_tests.cs
+++ Client/src/content/life_3_tests.cs
@@ -1,17 +1,24 @@
 static void RunTests()
 {
-        TestNeighbourCells(30, new List<int> { 9, 10, 11, 29, 31, 49, 50, 51 });
+    try
+    {
+        string fn = nameof(AdjustedNeighbourCells);
+        testAdjustedNeighbourCells(399, new List<int> { 378, 379, 380, 398, 0, 18, 19, 20 });
+        testAdjustedNeighbourCells(380, new List<int> { 359, 360, 361, 379, 381, 399, 0, 1 });
+        testAdjustedNeighbourCells(0, new List<int> { 379, 380, 381, 399, 1, 19, 20, 21 });
+        testAdjustedNeighbourCells(19, new List<int> { 398, 399, 0, 18, 20, 38, 39, 40 });
         AllTestsPassed();
+    }
+    catch (TestFailure) { }
 }
 
-private static void TestNeighbourCells(int cell, List<int> expected)
+private static void testAdjustedNeighbourCells(int cell, List<int> expected)
 {
-    var n = NeighbourCells(cell);
-    string fn = nameof(NeighbourCells);
-    var msg = fail + $"{fn}({cell}).";
-    AssertTrue(fn, cell.ToString(), n.Count == 8, msg + $" Expected: 8 elements Actual: {n.Count}");
+    var n = AdjustedNeighbourCells(cell);
+    string fn = nameof(AdjustedNeighbourCells);
+    AssertTrue(fn, cell.ToString(), n.Count == 8, $" Expected: 8 elements Actual: {n.Count}");
     foreach (int val in expected)
     {
-        AssertTrue(fn, cell.ToString(), n.Contains(val), msg + $" List does not contain: {val}");
+        AssertTrue(fn, cell.ToString(), n.Contains(val), $"List does not contain: {val}");
     }
 }
