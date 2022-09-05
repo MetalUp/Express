4b343a238eca6b29e31cfea9b33b45083d20f435
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-09-05 12:54:10 +0100

    Initial migration setup

---
+++ Administration System/Administration.Model/AdminDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Model; 
+
+public class AdminDbContextFactory : IDesignTimeDbContextFactory<AdminDbContext>
+{
+    public AdminDbContext CreateDbContext(string[] args)
+    {
+        var db = new AdminDbContext("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=ILEAdmin;Integrated Security=True;MultipleActiveResultSets=True");
+        //db.Create();
+        return db;
+    }
+}
