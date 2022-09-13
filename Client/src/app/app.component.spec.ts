255e0792522e1e70c5c495a4c405c7e2abc9c38c
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-09-13 11:06:29 +0100

    #8 updating and fixing tests

--- Client/src/app/app.component.spec.ts
+++ Client/src/app/app.component.spec.ts
@@ -1,9 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService, ConfigService } from '@nakedobjects/services';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let configServiceSpy: jasmine.SpyObj<ConfigService>;
+
+
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['handleAuthentication']);
+    routerSpy = jasmine.createSpyObj('Router', [], { url: "/dashboard/home" });
+    configServiceSpy = jasmine.createSpyObj('ConfigService', [], { loading: 0 });
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -11,27 +22,38 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceSpy
+        },
+        {
+          provide: Router,
+          useValue: routerSpy
+        },
+        {
+          provide: ConfigService,
+          useValue: configServiceSpy
+        }
+      ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.componentInstance;
-    // expect(app).toBeTruthy();
-
-    expect(true).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'ILEClient'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('ILEClient');
-  // });
+  it(`should call authenticate`, () => {
+    TestBed.createComponent(AppComponent);
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('ILEClient app is running!');
-  // });
+    expect(authServiceSpy.handleAuthentication).toHaveBeenCalled();
+  });
+
+  it('should indicate dashboard', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.isDashboard()).toBeTrue();
+  });
 });
