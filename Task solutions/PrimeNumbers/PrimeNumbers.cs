b3ed2359933acd8d70cecb37b81a2f81783d6074
Richard Pawson <rpawson@nakedobjects.net>
2022-09-20 09:57:06 +0100

    Content only

--- Task solutions/PrimeNumbers/PrimeNumbers.cs
+++ Task solutions/PrimeNumbers/PrimeNumbers.cs
@@ -8,7 +8,7 @@ namespace PrimeNumbers
 {
     public static class PrimeNumbers
     {
-        public static List<int> Primes(int upTo) => Enumerable.Range(2, upTo + 1).Select(n => IsPrime(n)).ToList();
+        public static List<int> Primes(int upTo) => Enumerable.Range(2, upTo + 1).Where(n => IsPrime(n)).ToList();
 
         public static bool IsPrime(int n) => !Enumerable.Range(2, n / 2).Any(f => IsFactor(f, n));
 
