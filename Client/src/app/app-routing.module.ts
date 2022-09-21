4a83f9e4abcf5f2582f76948f4c81272b5da3a37
Richard Pawson <rpawson@nakedobjects.net>
2022-09-21 16:27:46 +0100

    Change AppRouting to point to new default task (id = 30)

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -112,9 +112,9 @@ const routes: Routes = [
         component: CallbackComponent
     },
     { path: 'task/:id', component: TaskViewComponent, canActivate: [AuthService] },
-    { path: 'task',  redirectTo: 'task/9', pathMatch: 'full'},
+    { path: 'task',  redirectTo: 'task/30', pathMatch: 'full'},
     { path: 'dashboard',  redirectTo: 'dashboard/home', pathMatch: 'full'},
-    { path: '**', redirectTo: 'task/9', pathMatch: 'full' }
+    { path: '**', redirectTo: 'task/30', pathMatch: 'full' }
 ];
 
 @NgModule({
