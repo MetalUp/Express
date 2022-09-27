404ce2c3af6200a556dc3d3ee9fc96d7bbb85513
Richard Pawson <rpawson@nakedobjects.net>
2022-09-27 10:33:08 +0100

    Admin Model - Student-Group n:n now working (domain code refinement made)

--- Administration System/Administration.Model/Functions/Group_Functions.cs
+++ Administration System/Administration.Model/Functions/Group_Functions.cs
@@ -3,13 +3,10 @@
     public static class Group_Functions
     {
 
-        public static IContext AddStudent(this Group group, User student, IContext context)
-        {
-            var s2 = new List<User>(group.Students);
-            s2.Add(student);
-            var g2 = new Group(group) {Students = s2};
-            return context.WithUpdated(group, g2);
-        }
+        public static IContext AddStudent(this Group group, User student, IContext context) =>
+            context.WithUpdated(group, 
+                new Group(group) { Students = group.Students.Append(student).ToList() });
+
 
         public static string ValidateAddStudent(this Group group, User student, IContext context) =>
             !student.HasRole(Role.Student) ? "User is not a Student" :
@@ -19,8 +16,7 @@
         {
             var s2 = new List<User>(group.Students);
             s2.Remove(student);
-            var g2 = new Group(group) { Students = s2 };
-            return context.WithUpdated(group, g2);
+            return context.WithUpdated(group, new Group(group) { Students = s2 });
         }
 
         public static List<User> Choices1RemoveStudent(this Group group, User student, IContext context) =>
