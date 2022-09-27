89cc6a1002da48a83159c0e42a70db550d18ddcf
Richard Pawson <rpawson@nakedobjects.net>
2022-09-27 13:11:00 +0100

    #65

--- Administration System/Administration.Model/Authorization/AssignmentAuthorizer.cs
+++ Administration System/Administration.Model/Authorization/AssignmentAuthorizer.cs
@@ -9,19 +9,24 @@ namespace Model.Authorization
             var me = Users.Me(context);
             return me.Role   switch
             {
+                Role.Root => true,
                 >= Role.Teacher => TeacherAuthorization(a, memberName,me, context),
                 Role.Student => StudentAuthorization(a, memberName, me, context),
                 _ => false
             };
         }
 
-        private bool TeacherAuthorization(Assignment a, string memberName, User me, IContext context) =>
-            a.AssignedById == me.Id ||
+        private static bool TeacherAuthorization(Assignment a, string memberName, User me, IContext context) =>
+            IsAssignedToMe(a, me) ||
+            IsAssignedByOrToAnyoneInMyOrganisation(a, me) && !Helpers.MatchesOneOf(memberName, nameof(Assignment.Link));
+            
+        private static bool StudentAuthorization(Assignment a, string memberName, User me, IContext context) =>
+            IsAssignedToMe(a, me) && Helpers.MemberIsProperty(a, memberName);
+
+        private static bool IsAssignedToMe(Assignment a, User me) => a.AssignedToId == me.Id;
+
+        private static bool IsAssignedByOrToAnyoneInMyOrganisation(Assignment a, User me) =>
             a.AssignedBy.OrganisationId == me.OrganisationId ||
             a.AssignedTo.OrganisationId == me.OrganisationId;
-
-        private bool StudentAuthorization(Assignment a, string memberName, User me, IContext context) =>
-            a.AssignedToId == me.Id && Helpers.MemberIsProperty(a, memberName);
     }
-
 }
