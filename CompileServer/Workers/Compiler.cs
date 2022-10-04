ef937d7c46cc35fed23cce5382089f70d01a149c
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-10-04 10:41:33 +0100

    #11 add vb

--- CompileServer/Workers/Compiler.cs
+++ CompileServer/Workers/Compiler.cs
@@ -6,6 +6,7 @@ public static class Compiler {
     public static (RunResult, byte[]) Compile(RunSpec runSpec) {
         return runSpec.language_id switch {
             "csharp" => CSharpCompiler.Compile(runSpec),
+            "vb" => VisualBasicCompiler.Compile(runSpec),
             _ => (new RunResult { outcome = Outcome.IllegalSystemCall, cmpinfo = $"Unknown language: {runSpec.language_id}"}, Array.Empty<byte>())
         };
     }
