873c070d80e29f36f0c08541109b4843607e7edf
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-10-05 09:17:45 +0100

    #11 more azure work

--- CompileServer/Controllers/RunsController.cs
+++ CompileServer/Controllers/RunsController.cs
@@ -7,14 +7,18 @@ namespace CompileServer.Controllers;
 [Route("restapi/[controller]")]
 public class RunsController : ControllerBase {
     private readonly ILogger<RunsController> logger;
-    public static string PythonPath { get; set; } = "";
-    public static string JavaPath { get; set; } = "";
+    public static string PythonPath { get; private set; } = "";
+    public static string JavaPath { get; private set; } = "";
+
+    private const string PythonPathName = "PythonPath";
+    private const string JavaPathName = "JavaPath";
+    private const string CompileServerName = "CompileServer";
 
     public RunsController(ILogger<RunsController> logger, IConfiguration configuration) {
         this.logger = logger;
 
-        PythonPath = configuration.GetSection("CompileServer").GetSection("PythonPath").Value;
-        JavaPath = configuration.GetSection("CompileServer").GetSection("JavaPath").Value;
+        PythonPath = Environment.GetEnvironmentVariable(PythonPathName) ?? configuration.GetSection(CompileServerName).GetSection(PythonPathName).Value;
+        JavaPath = Environment.GetEnvironmentVariable(JavaPathName) ?? configuration.GetSection(CompileServerName).GetSection(JavaPathName).Value;
     }
 
     [HttpPost]
