ea0c693c69f395dc33d94fda5bc37576d9090806
Richard Pawson <rpawson@nakedobjects.net>
2022-10-12 10:58:17 +0100

    #91 add method to edit an existing File as a string

--- Administration System/Administration.Model/Functions/File_Functions.cs
+++ Administration System/Administration.Model/Functions/File_Functions.cs
@@ -1,6 +1,13 @@
-﻿namespace Model.Functions
+﻿using System.Text;
+
+namespace Model.Functions
 {
     public static class File_Functions
     {
+        public static IContext EditContentAsString(this File file, string content, IContext context) =>
+            context.WithUpdated(file, new File(file) {Content = Encoding.ASCII.GetBytes(content)});
+
+        public static string Default1EditContentAsString(this File file) =>
+            Encoding.Default.GetString(file.Content);
     }
 }
