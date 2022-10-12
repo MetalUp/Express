90317d8cb062d19a8f21c826bb92c3dca803cb88
Richard Pawson <rpawson@nakedobjects.net>
2022-10-12 11:17:47 +0100

    #91 cont.

--- Administration System/Administration.Model/Functions/File_Functions.cs
+++ Administration System/Administration.Model/Functions/File_Functions.cs
@@ -1,13 +1,18 @@
-﻿using System.Text;
+﻿using NakedFramework.Value;
+using System.Text;
 
 namespace Model.Functions
 {
     public static class File_Functions
     {
+        public static IContext ReloadFromExternalFile(this File file, FileAttachment externalFile, IContext context) =>
+            context.WithUpdated(file, new File(file) { Content = externalFile.GetResourceAsByteArray(), Name=externalFile.Name, Mime = externalFile.MimeType });
+
         public static IContext EditContentAsString(this File file, [MultiLine(20)] string content, IContext context) =>
             context.WithUpdated(file, new File(file) {Content = Encoding.ASCII.GetBytes(content)});
 
         public static string Default1EditContentAsString(this File file) =>
             Encoding.Default.GetString(file.Content);
+
     }
 }
