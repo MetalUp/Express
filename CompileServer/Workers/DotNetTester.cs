95303857686617ec5990c15f0230e93074def033
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-10-26 08:58:53 +0100

    #102 temp ignore tests until paths sorted

--- CompileServer/Workers/DotNetTester.cs
+++ CompileServer/Workers/DotNetTester.cs
@@ -14,25 +14,9 @@ public static class DotNetTester {
         const string tempFileName = "compiled.dll";
         var file = $"{Path.GetTempPath()}{tempFileName}";
 
-        File.Delete(file);
-
         File.WriteAllBytes(file, compiledAssembly);
 
-        LoadIfNotInTemp("testhost.dll");
         LoadIfNotInTemp("Microsoft.TestPlatform.CoreUtilities.dll");
-        LoadIfNotInTemp("Microsoft.TestPlatform.PlatformAbstractions.dll");
-        LoadIfNotInTemp("Microsoft.TestPlatform.CrossPlatEngine.dll");
-        LoadIfNotInTemp("Microsoft.TestPlatform.CommunicationUtilities.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.ObjectModel.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.Common.dll");
-        LoadIfNotInTemp("Newtonsoft.Json.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.TestFramework.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interface.dll");
-        LoadIfNotInTemp("Microsoft.TestPlatform.AdapterUtilities.dll");
-        LoadIfNotInTemp("Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.dll");
-        LoadIfNotInTemp("NuGet.Frameworks.dll");
 
         if (!File.Exists($"{Path.GetTempPath()}compiled.runtimeconfig.json")) {
             var rtg = @"{
