236ee0d5932c345d680efe4621a5a2c8fc0e8715
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-10-26 15:09:38 +0100

    #102 VB tests

--- CompileServerTest/Workers/VisualBasicCompilerTest.cs
+++ CompileServerTest/Workers/VisualBasicCompilerTest.cs
@@ -28,6 +28,34 @@ public class VisualBasicCompilerTest {
             End Sub 
           End Module";
 
+    private const string TestCodeOk =
+        @"
+        Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+        Namespace ExpressTests
+            <TestClass>
+            Public Class Tests
+                <TestMethod>
+                Public Sub ATest()
+                    Assert.IsTrue(True)
+                End Sub
+            End Class
+        End Namespace";
+
+    private const string TestCodeFail =
+        @"
+        Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+        Namespace ExpressTests
+            <TestClass>
+            Public Class Tests
+                <TestMethod>
+                Public Sub ATest()
+                    Assert.IsTrue(False)
+                End Sub
+            End Class
+        End Namespace";
+
     [TestMethod]
     public void TestVersion() {
         var csv = VisualBasicCompiler.GetNameAndVersion();
@@ -70,5 +98,31 @@ public class VisualBasicCompilerTest {
 
         Assert.IsNotNull(rr);
         rr.AssertRunResult(Outcome.RunTimeError, "", "", "Input string was not in a correct format.");
+    }
+
+    [TestMethod]
+    public void TestCompileAndTestOk()
+    {
+        var runSpec = VisualBasicRunSpec(TestCodeOk);
+        var rr = Handler.CompileAndTest(runSpec).Result.Value;
+        Assert.IsNotNull(rr);
+        Assert.AreEqual(Outcome.Ok, rr.outcome);
+        Assert.AreEqual("", rr.cmpinfo);
+        Assert.IsTrue(rr.stdout.Contains("Passed!  - Failed:     0, Passed:     1, Skipped:     0, Total:     1"), rr.stdout);
+        Assert.AreEqual("", rr.stderr);
+        Assert.AreEqual("", rr.run_id);
+    }
+
+    [TestMethod]
+    public void TestCompileAndTestFail()
+    {
+        var runSpec = VisualBasicRunSpec(TestCodeFail);
+        var rr = Handler.CompileAndTest(runSpec).Result.Value;
+        Assert.IsNotNull(rr);
+        Assert.AreEqual(Outcome.Ok, rr.outcome);
+        Assert.AreEqual("", rr.cmpinfo);
+        Assert.IsTrue(rr.stdout.Contains("Failed!  - Failed:     1, Passed:     0, Skipped:     0, Total:     1"), rr.stdout);
+        Assert.AreEqual("", rr.stderr);
+        Assert.AreEqual("", rr.run_id);
     }
 }
