a2ccbe73dad94f5f1d6535dfdb2e5474735c0ffe
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-11-08 17:01:34 +0000

    #114 more tests

--- Client/src/app/services/registered.service.spec.ts
+++ Client/src/app/services/registered.service.spec.ts
@@ -1,4 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TimePickerComponent } from '@nakedobjects/gemini';
+import { UserRepresentation } from '@nakedobjects/restful-objects';
 import { AuthService, ContextService } from '@nakedobjects/services';
 
 import { RegisteredService } from './registered.service';
@@ -9,14 +11,25 @@ describe('RegisteredService', () => {
   let contextServiceSpy: jasmine.SpyObj<ContextService>;
 
   beforeEach(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['load'], {});
-    contextServiceSpy = jasmine.createSpyObj('ContextService', ['load'], {});
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['canActivate'], {});
+    contextServiceSpy = jasmine.createSpyObj('ContextService', ['getUser'], {});
 
     TestBed.configureTestingModule({});
     service = new RegisteredService(authServiceSpy, contextServiceSpy);
+
+    authServiceSpy.canActivate.and.returnValue(true);
+    contextServiceSpy.getUser.and.returnValue(Promise.resolve({ userName: () => "name"} as UserRepresentation));
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should handle canActivate', fakeAsync(() => {
+    service.canActivate().then(b => expect(b).toBeTrue());
+    tick();
+    expect(service.registered).toBeTrue();
+
+  }));
+
 });
