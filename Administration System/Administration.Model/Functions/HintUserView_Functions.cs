4769a5176ef1c7806d3919b89d51bdc124c2718c
Richard Pawson <rpawson@nakedobjects.net>
2022-11-15 18:01:15 +0000

    Implement CreateFromKeys for HintUserView

--- Administration System/Administration.Model/Functions/HintUserView_Functions.cs
+++ Administration System/Administration.Model/Functions/HintUserView_Functions.cs
@@ -7,7 +7,9 @@ public static class HintUserView_Functions
     public static string[] DeriveKeys(this HintUserView target) =>
         new[] { target.TaskId.ToString(), target.HintNo.ToString() };
 
-    public static HintUserView CreateFromKeys(string[] keys, IContext context) => throw new NotImplementedException();
-    //TaskAccess.GetHint(Convert.ToInt32(keys[0]), Convert.ToInt32(keys[1]), context)
+    public static HintUserView CreateFromKeys(string[] keys, IContext context) =>
+       TaskAccess.GetHint(Convert.ToInt32(keys[0]), Convert.ToInt32(keys[1]), context).Item1; //Safe to ignore returned IContext,
+           //because this method only ever called if the view has been previously accessed
+       
 
 }
