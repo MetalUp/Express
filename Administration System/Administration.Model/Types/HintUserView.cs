4259ac62a0cc1d19d49ac2d1c5522f8955416113
Richard Pawson <rpawson@nakedobjects.net>
2022-11-16 13:34:09 +0000

    #125 Next & Previous Hint Ids not now nullable - 0 indicates 'none such'

--- Administration System/Administration.Model/Types/HintUserView.cs
+++ Administration System/Administration.Model/Types/HintUserView.cs
@@ -4,7 +4,7 @@ namespace Model.Types
     [ViewModel(typeof(HintUserView_Functions))]
     public class HintUserView
     {
-        public HintUserView(int taskId, int hintNo, string title, string contents, int? previousHintNo, int? nextHintNo, int costOfNextHint)
+        public HintUserView(int taskId, int hintNo, string title, string contents, int previousHintNo, int nextHintNo, int costOfNextHint)
         {
             TaskId = taskId;
             HintNo = hintNo;
@@ -23,9 +23,9 @@ namespace Model.Types
 
         public string Contents { get; init; }
 
-        public int? PreviousHintNo { get; init; } //Null indicates there is no previous hint
+        public int PreviousHintNo { get; init; } //0 indicates there is no previous hint
 
-        public int? NextHintNo { get; init; } //Null indicates there is no next hint
+        public int NextHintNo { get; init; } //0 indicates there is no next hint
 
         public int CostOfNextHint { get; init; }  //If zero means that the user can just navigate to it (because they have seen it before)
     }
