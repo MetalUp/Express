2ee29e27c85322e31e148575b8befe232fc6dd48
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-12-13 10:30:14 +0000

    #151 continue tooltips

--- Client/src/app/constants/tooltips.ts
+++ Client/src/app/constants/tooltips.ts
@@ -11,3 +11,17 @@ export const nextTaskDisabledTooltip = "disabled next tooltip";
 // new task button 
 export const newTaskEnabledTooltip = "enabled new tooltip";
 export const newTaskDisabledTooltip = "disabled new tooltip";
+
+// Expression pane
+
+// previous Expression arrow 
+export const previousExpressionEnabledTooltip = "enabled previous tooltip";
+export const previousExpressionDisabledTooltip = "disabled previous tooltip";
+
+// next Expression arrow 
+export const nextExpressionEnabledTooltip = "enabled next tooltip";
+export const nextExpressionDisabledTooltip = "disabled next tooltip";
+
+// submit Expression button 
+export const submitExpressionEnabledTooltip = "enabled submit tooltip";
+export const submitExpressionDisabledTooltip = "disabled submit tooltip";
