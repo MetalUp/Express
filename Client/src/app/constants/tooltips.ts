2c92b3f905fce88b719ec2f559a8eabf1cd41017
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-12-13 10:21:15 +0000

    #151 improve tooltips

---
+++ Client/src/app/constants/tooltips.ts
@@ -0,0 +1,13 @@
+// Task description pane
+
+// previous task arrow 
+export const previousTaskEnabledTooltip = "enabled previous tooltip";
+export const previousTaskDisabledTooltip = "disabled previous tooltip";
+
+// next task arrow 
+export const nextTaskEnabledTooltip = "enabled next tooltip";
+export const nextTaskDisabledTooltip = "disabled next tooltip";
+
+// new task button 
+export const newTaskEnabledTooltip = "enabled new tooltip";
+export const newTaskDisabledTooltip = "disabled new tooltip";
