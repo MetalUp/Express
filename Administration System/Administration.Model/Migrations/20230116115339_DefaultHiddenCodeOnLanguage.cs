1da5b1ac31cfc6c4d1a982bf992326a23f1f66cc
Richard Pawson <rpawson@nakedobjects.net>
2023-01-16 11:54:53 +0000

    #163 add default hidden code to language & use in Project

---
+++ Administration System/Administration.Model/Migrations/20230116115339_DefaultHiddenCodeOnLanguage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    public partial class DefaultHiddenCodeOnLanguage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultHiddenCode",
+                table: "Languages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultHiddenCode",
+                table: "Languages");
+        }
+    }
+}
