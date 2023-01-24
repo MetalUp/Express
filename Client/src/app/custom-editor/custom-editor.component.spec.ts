9a4243fc43c5ef4dfb6f6d6d1e090dba82476626
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-01-24 13:13:16 +0000

    #95 start custom editor test

--- Client/src/app/custom-editor/custom-editor.component.spec.ts
+++ Client/src/app/custom-editor/custom-editor.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { FileService } from '../services/file.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CustomEditorComponent } from './custom-editor.component';
+import { Subject } from 'rxjs';
 
 describe('CustomEditorComponent', () => {
   let component: CustomEditorComponent;
   let fixture: ComponentFixture<CustomEditorComponent>;
 
+
+  let fileServiceSpy: jasmine.SpyObj<FileService>;
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+  let locationSpy: jasmine.SpyObj<Location>;
+  let mapSub = new Subject<ParamMap>();
+
+  activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['navigate'], { paramMap: mapSub });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomEditorComponent ]
+      declarations: [CustomEditorComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRouteSpy
+        },
+        {
+          provide: Location,
+          useValue: locationSpy
+        },
+        {
+          provide: FileService,
+          useValue: fileServiceSpy
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
-    // fixture = TestBed.createComponent(CustomEditorComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(CustomEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(true).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
