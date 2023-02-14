4ad6a1b531857e8cd3fb32546618358460bedf0c
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-02-14 10:06:08 +0000

    #122 tidy

--- CompileServer/Workers/HaskellCompiler.cs
+++ CompileServer/Workers/HaskellCompiler.cs
@@ -34,44 +34,33 @@ public static class HaskellCompiler {
         return result;
     }
 
-    static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
-    {
+    private static void CopyDirectory(string sourceDir, string destinationDir) {
         // Get information about the source directory
         var dir = new DirectoryInfo(sourceDir);
 
-        // Check if the source directory exists
-        if (!dir.Exists)
-            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
-
         // Cache directories before we start copying
-        DirectoryInfo[] dirs = dir.GetDirectories();
+        var dirs = dir.GetDirectories();
 
         // Create the destination directory
         Directory.CreateDirectory(destinationDir);
 
         // Get the files in the source directory and copy to the destination directory
-        foreach (FileInfo file in dir.GetFiles())
-        {
-            string targetFilePath = Path.Combine(destinationDir, file.Name);
+        foreach (var file in dir.GetFiles()) {
+            var targetFilePath = Path.Combine(destinationDir, file.Name);
             file.CopyTo(targetFilePath);
         }
 
-        // If recursive and copying subdirectories, recursively call this method
-        if (recursive)
-        {
-            foreach (DirectoryInfo subDir in dirs)
-            {
-                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
-                CopyDirectory(subDir.FullName, newDestinationDir, true);
-            }
+        foreach (var subDir in dirs) {
+            var newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+            CopyDirectory(subDir.FullName, newDestinationDir);
         }
     }
 
 
     internal static (RunResult, string) Compile(RunSpec runSpec) {
 
-        CopyDirectory($"{runSpec.Options.HaskellPath}\\HUnit-1.6.2.0\\src", runSpec.TempDir, true);
-        CopyDirectory($"{runSpec.Options.HaskellPath}\\call-stack-0.4.0\\src", runSpec.TempDir, true);
+        CopyDirectory($"{runSpec.Options.HaskellPath}\\HUnit-1.6.2.0\\src", runSpec.TempDir);
+        CopyDirectory($"{runSpec.Options.HaskellPath}\\call-stack-0.4.0\\src", runSpec.TempDir);
 
         const string tempFileName = "temp.hs";
         var file = @$"{runSpec.TempDir}{tempFileName}";
