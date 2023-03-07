df5aa544a7dff82d4b5a434582d3a99b9d078074
Richard Pawson <rpawson@nakedobjects.net>
2023-03-07 10:15:23 +0000

    Update Language_Functions.cs

--- Administration System/Administration.Model/Functions/Language_Functions.cs
+++ Administration System/Administration.Model/Functions/Language_Functions.cs
@@ -24,6 +24,27 @@
  IContext context) =>
      context.WithUpdated(language, new(language) { RegExRulesFileId = regExRulesFile.Id, RegExRulesFile = regExRulesFile });
 
+        [Edit]
+        public static IContext EditName(this Language language, string name, IContext context) =>
+            context.WithUpdated(language, new(language) { Name = name });
+
+        [Edit]
+        public static IContext EditAlphaName(this Language language, string alphaName, IContext context) =>
+    context.WithUpdated(language, new(language) { AlphaName = alphaName });
+
+        [Edit]
+        public static IContext EditVersion(this Language language, string version, IContext context) =>
+context.WithUpdated(language, new(language) { Version = version });
+
+
+        [Edit]
+        public static IContext EditFileExtension(this Language language, string fileExtension, IContext context) =>
+context.WithUpdated(language, new(language) { FileExtension = fileExtension });
+
+        [Edit]
+        public static IContext EditMimeType(this Language language, string mimeType, IContext context) =>
+context.WithUpdated(language, new(language) { MIMEType = mimeType });
+
         #endregion
     }
 }
