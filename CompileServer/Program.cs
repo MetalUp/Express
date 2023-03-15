4bb7c43e218399e1edc317984566f5363c3002e5
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-15 14:28:44 +0000

    #157 remove cors from compile server and tidy

--- CompileServer/Program.cs
+++ CompileServer/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using static System.Net.WebRequestMethods;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,16 +10,10 @@ builder.Services.AddAuthentication(options => {
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options => {
     options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-    //options.Audience = builder.Configuration["Auth0:Audience"];
-    //options.TokenValidationParameters.NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
-
-
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
+    options.TokenValidationParameters = new TokenValidationParameters {
         NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
         ValidateIssuer = true,
-        ValidAudiences = new List<string> 
-        {
+        ValidAudiences = new List<string> {
             "https://metalupadminserver.azurewebsites.net",
             "https://metalupadminserverdevelopment.azurewebsites.net",
             "https://metalupadminservertest.azurewebsites.net"
@@ -29,31 +23,8 @@ builder.Services.AddAuthentication(options => {
 
 builder.Services.AddControllers();
 
-//builder.Services.AddCors(options => {
-//    options.AddPolicy("_myAllowSpecificOrigins", builder => {
-//        builder
-//            .WithOrigins(
-//                "http://localhost:5001",
-//                "http://localhost:49998",
-//                "https://express.metalup.org")
-//            .AllowAnyHeader()
-//            .WithExposedHeaders("Warning", "ETag", "Set-Cookie")
-//            .AllowAnyMethod()
-//            .AllowCredentials();
-//    });
-//});
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) { }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.UseCors("_myAllowSpecificOrigins");
-
 app.Run();
