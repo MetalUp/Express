24ed367dedde299ba409ef96b3cd71f85d4a17c4
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-20 13:41:32 +0000

    #157 tweak login

--- Administration System/Administration.Test/ExpressTestCase.cs
+++ Administration System/Administration.Test/ExpressTestCase.cs
@@ -1,23 +1,21 @@
 using System.Security.Principal;
-using NakedFramework.Architecture.Framework;
-using NakedFramework.Persistor.EFCore.Util;
+using Microsoft.Extensions.DependencyInjection;
+using NakedFramework.Architecture.Component;
 using NakedFramework.RATL.TestCase;
 using User = Model.Types.User;
 
 namespace Test;
 
 public abstract class ExpressTestCase : BaseRATLNUnitTestCase {
+    protected static IIdentity TestIdentity { get; } = new GenericIdentity("");
+
+    protected new IPrincipal TestPrincipal { get; set; } = new GenericPrincipal(TestIdentity, new string[] { });
     protected Home GetHome() => ROSIApi.GetHome(new Uri("http://localhost/"), TestInvokeOptions()).Result;
 
     protected DomainObject GetObject<T>(string key) => GetObject(FullName<T>(), key);
 
-    protected static IIdentity TestIdentity { get; } = new GenericIdentity("");
-
-    protected new IPrincipal TestPrincipal { get; set; } = new GenericPrincipal(TestIdentity, new string[]{});
-
     protected void LogInAs(string name) {
-        TestPrincipal = new GenericPrincipal(new GenericIdentity(name), new String [] { });
+        var user = ServiceScope.ServiceProvider.GetService<IObjectPersistor>()?.Instances<User>().SingleOrDefault(u => u.Name == name);
+        TestPrincipal = new GenericPrincipal(new GenericIdentity(user?.UserName ?? ""), new string[] { });
     }
-
-
 }
