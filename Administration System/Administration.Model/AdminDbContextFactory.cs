7444538cbb0b94349fc93fff009445478857760d
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-27 10:33:16 +0100

    #186 add cs as secret to model

---
+++ Administration System/Administration.Model/AdminDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Model; 
+
+public class AdminDbContextFactory : IDesignTimeDbContextFactory<AdminDbContext> {
+    public AdminDbContext CreateDbContext(string[] args) {
+        var configuration = new ConfigurationBuilder()
+                            .AddUserSecrets<AdminDbContext>()
+                            .Build();
+
+        var cs = Environment.GetEnvironmentVariable("connection_string") ?? configuration["ConnectionString"];
+        var optionsBuilder = new DbContextOptionsBuilder<AdminDbContext>();
+        optionsBuilder.UseSqlServer(cs);
+
+        return new AdminDbContext(optionsBuilder.Options);
+    }
+}
