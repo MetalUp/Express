2fdd4d944fdddc8d9f460981f5342b2d539406d4
Richard Pawson <rpawson@nakedobjects.net>
2023-03-28 13:42:59 +0100

    #192 another bug fix

--- Administration System/Administration.Model/Functions/TaskUserView_Functions.cs
+++ Administration System/Administration.Model/Functions/TaskUserView_Functions.cs
@@ -5,7 +5,7 @@ namespace Model.Functions;
 public static class TaskUserView_Functions
 {
     public static string[] DeriveKeys(this TaskUserView target) => 
-        new[] { target.Task.Id.ToString()};
+        new[] { target.Task  == null ? "0" :target.Task.Id.ToString()};
 
     public static TaskUserView CreateFromKeys(string[] keys, IContext context) => 
         TaskAccess.GetTask(Convert.ToInt32(keys[0]), context);
