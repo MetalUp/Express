8bdb2a8fed38e808a4c10e57336d1047440fb944
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-28 11:59:44 +0100

    #173 fix test

--- Client/src/app/app.component.spec.ts
+++ Client/src/app/app.component.spec.ts
@@ -71,7 +71,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     app.ngOnInit();
-    expect(app.loading).toBeFalse();
+    expect(app.loading).toBeTrue();
     loadingSubj.next(1);
     expect(app.loading).toBeTrue();
     loadingSubj.next(0);
