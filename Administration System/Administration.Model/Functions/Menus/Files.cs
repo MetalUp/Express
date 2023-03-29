b3dfdaea7f6a0226422123e1d4747bf8653b3d0f
Richard Pawson <rpawson@nakedobjects.net>
2023-03-29 14:48:14 +0100

    Minor change to ordering of Common Files

--- Administration System/Administration.Model/Functions/Menus/Files.cs
+++ Administration System/Administration.Model/Functions/Menus/Files.cs
@@ -67,6 +67,6 @@ namespace Model.Functions.Menus
         [RenderEagerly]
         [TableView(false, nameof(File.Language), nameof(File.ContentType), nameof(File.Name), nameof(File.UniqueRef))]
         public static IQueryable<File> FilesWithUniqueRef(IContext context) =>
-            context.Instances<File>().Where(f => f.UniqueRef != null).OrderBy(f => f.LanguageId).ThenBy(f => f.ContentType);
+            context.Instances<File>().Where(f => f.UniqueRef != null);
     }
 }
