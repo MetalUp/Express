b7b36786b89c729e9f3f423e92cbc6945ff79aba
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-31 09:28:55 +0100

    #196 use logging

--- Administration System/Batch/Handler.cs
+++ Administration System/Batch/Handler.cs
@@ -1,26 +1,30 @@
-﻿using Model.Functions.Services;
+﻿using Microsoft.Extensions.Logging;
+using Model.Functions.Services;
 using NakedFramework.Facade.Contexts;
 using NakedFramework.Facade.Interface;
 
 namespace Batch;
 
 internal class Handler {
-    public Handler(IFrameworkFacade frameworkFacade) => FrameworkFacade = frameworkFacade;
+    public Handler(IFrameworkFacade frameworkFacade, ILogger<Handler> logger) {
+        Logger = logger;
+        FrameworkFacade = frameworkFacade;
+    }
 
+    private ILogger Logger { get; }
     private IFrameworkFacade FrameworkFacade { get; }
 
     public void Run() {
         try {
-            Console.WriteLine("Starting Batch");
+            Logger.LogInformation("Starting Batch");
             FrameworkFacade.Start();
             var args = new ArgumentsContextFacade { ExpectedActionType = MethodType.NonIdempotent, Values = new Dictionary<string, object>() };
             FrameworkFacade.ExecuteMenuAction(typeof(BatchProcessing).FullName, nameof(BatchProcessing.UpdateFiles), args);
             FrameworkFacade.End(true);
-            Console.WriteLine("Batch complete with no errors");
+            Logger.LogInformation("Batch complete with no errors");
         }
         catch (Exception e) {
-            Console.WriteLine("Batch failed with errors");
-            Console.WriteLine(e);
+            Logger.LogError("Batch failed with error", e);
             throw;
         }
     }
