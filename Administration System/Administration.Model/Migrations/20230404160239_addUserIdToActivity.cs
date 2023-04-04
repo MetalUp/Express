f051121375470a8cb5d7c066c9393014291ff8c3
Richard Pawson <rpawson@nakedobjects.net>
2023-04-04 17:03:57 +0100

    Add migration required by last check-in (not applied yet)

---
+++ Administration System/Administration.Model/Migrations/20230404160239_addUserIdToActivity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUserIdToActivity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Activities",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Activities");
+        }
+    }
+}
