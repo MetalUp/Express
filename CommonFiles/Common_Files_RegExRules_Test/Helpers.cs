5e9321aef466ffafc909dcf18ffaca6cf88a10cd
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-05-10 14:00:56 +0100

    Regex test helpers implemented

--- CommonFiles/Common_Files_RegExRules_Test/Helpers.cs
+++ CommonFiles/Common_Files_RegExRules_Test/Helpers.cs
@@ -1,18 +1,72 @@
-﻿namespace RegExRules_Test
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
+namespace RegExRules_Test;
+
+internal static class Helpers
 {
-    internal class Helpers
+    //Applies appropriate RegExRules to code. If all tests pass, returns null, else returns failure message
+    internal static string? ValidateCode(string regexFile, string code)
     {
-        //Applies appropriate RegExRules to code. If all tests pass, returns null, else returns failure message
-        internal static string? ValidateCode(string regexFile, string code)
-        {
-            throw new NotImplementedException();
-        }
-
-        //Applies appropriate RegExRules to expression. If all tests pass, returns null, else returns failure message
-        internal static string ValidateExpression(string regexFile, string expression)
-        {
-            throw new NotImplementedException();
-        }
-
+        var (mustMatchRules, mustNotContainRules) = GetRules(regexFile, "functions");
+        return ApplyRules(code, mustMatchRules, mustNotContainRules);
     }
+
+    //Applies appropriate RegExRules to expression. If all tests pass, returns null, else returns failure message
+    internal static string? ValidateExpression(string regexFile, string expression)
+    {
+        var (mustMatchRules, mustNotContainRules) = GetRules(regexFile, "expressions");
+        return ApplyRules(expression, mustMatchRules, mustNotContainRules);
+    }
+
+    private static string? ApplyRules(string code, IEnumerable<JArray> mustMatchRules, IEnumerable<JArray> mustNotContainRules)
+    {
+        foreach (var rule in mustMatchRules)
+        {
+            var pattern = rule[0].Value<string>();
+            var msg = rule[1].Value<string>();
+
+            if (!Regex.Match(code, pattern).Success)
+            {
+                return msg;
+            }
+        }
+
+        foreach (var rule in mustNotContainRules)
+        {
+            var pattern = rule[0].Value<string>();
+            var msg = rule[1].Value<string>();
+
+            var matches = Regex.Match(code, pattern);
+
+            if (matches.Success)
+            {
+                for (var i = 0; i < matches.Groups.Count; i++)
+                {
+                    msg = msg.Replace($"{{{i}}}", matches.Groups[i].ToString());
+                }
+
+                return msg;
+            }
+        }
+
+        return null;
+    }
+
+    private static (IEnumerable<JArray>, IEnumerable<JArray>) GetRules(string regexFile, string ruleType)
+    {
+        var rootDir = Directory.GetCurrentDirectory().Replace(@"\Common_Files_RegExRules_Test\bin\Debug\net6.0", "");
+        var path = Path.Combine(rootDir, regexFile);
+
+        var json = File.ReadAllText(path);
+        var asJson = JObject.Parse(json);
+        var mustMatch = asJson["CodeMustMatch"];
+        var mustNotContain = asJson["CodeMustNotContain"];
+
+        var mustMatchRules = GetRules(ruleType, mustMatch).Union(GetRules("both", mustMatch));
+        var mustNotContainRules = GetRules(ruleType, mustNotContain).Union(GetRules("both", mustNotContain));
+        return (mustMatchRules, mustNotContainRules);
+    }
+
+    private static IEnumerable<JArray> GetRules(string ruleType, JToken mustMatch) => ((JArray)mustMatch[ruleType]).Cast<JArray>();
 }
--- CommonFiles/Common_Files_RegExRules_Test/RegExRules_CS_Test.cs
+++ CommonFiles/Common_Files_RegExRules_Test/RegExRules_CS_Test.cs
@@ -1,35 +1,37 @@
-namespace CommonFiles_CS_Test
+namespace CommonFiles_CS_Test;
+
+[TestClass]
+public class RegExRules_CS_Test
 {
-    [TestClass]
-    public class RegExRules_CS_Test
+    private const string CS = @"Common_Files_CS\Functions_regex.json"; //Appropriate relative path
+
+    [TestMethod]
+    public void ValidExpression()
     {
-        const string CS = @"..\Common_Files_CS\Functions_regex.json"; //Appropriate relative path
+        Assert.IsNull(ValidateExpression(CS, "3+4"));
+    }
 
-        [TestMethod]
-        public void ValidExpression()
-        {
-           //Assert.IsNull(ValidateExpression(CS, "3+4"));
-        }
+    [TestMethod]
+    public void ExpressionUsingVar()
+    {
+        Assert.AreEqual("Use of the keyword 'var' is not permitted", ValidateExpression(CS, "var a = 1"));
+    }
 
-        [TestMethod]
-        public void ExpressionUsingVar()
-        {
-           // Assert.AreEqual("Use of of var not allowed", 
-               // ValidateExpression(CS, "var a = 1")); //Illustration only - Not real message
-        }
+    [TestMethod]
+    public void ValidCode()
+    {
+        Assert.IsNull(ValidateCode(CS, "static int Foo(int x) => x*x;"));
+    }
 
-        [TestMethod]
-        public void ValidCode()
-        {
-           // Assert.IsNull(ValidateCode(CS, "static int Foo(int x) => x*x;"));
-        }
+    [TestMethod]
+    public void CodeUsingReturn()
+    {
+        Assert.AreEqual("Use of the keyword 'return' is not permitted", ValidateCode(CS, "static int Foo(int x) { return x*x; }"));
+    }
 
-        [TestMethod]
-        public void NonStaticFunction()
-        { 
-           // Assert.AreEqual("Function definitions must start with 'static'",
-            //    ValidateCode(CS,"int Foo(int x) => x*x;")); //Illustration only - Not real message
-        }
-
+    [TestMethod]
+    public void NonStaticFunction()
+    {
+        Assert.AreEqual("All functions should be: static <ReturnType> <NameStartingInUpperCase>(<parametersStartingLowerCase>) => <expression>;", ValidateCode(CS, "int Foo(int x) => x*x;"));
     }
 }
