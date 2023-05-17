10e06d800e9d38a8fc707d75ded45ff44d9a1f4a
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-05-17 13:08:42 +0100

    Improve batch logging

--- Administration System/Administration.Model/Services/BatchProcessing.cs
+++ Administration System/Administration.Model/Services/BatchProcessing.cs
@@ -1,18 +1,28 @@
-﻿namespace Model.Services;
+﻿using Microsoft.Extensions.Logging;
 
-public static class BatchProcessing
-{
+namespace Model.Services;
+
+public static class BatchProcessing {
     public static IContext UpdateFiles(IContext context) =>
         CommonFileDefinitions.defs.Aggregate(context, (c, cfd) => UpdateOrCreateNew(cfd, c));
 
-    static IContext UpdateOrCreateNew(File def, IContext context)
-    {
-        File f = PersistedFileIfExists(def, context);
-        return f == null ?
-            context.WithNew(def) :
-            context.WithUpdated(f, new File(def) { Id = f.Id });
+    private static IContext UpdateOrCreateNew(File def, IContext context) {
+        var logger = context.GetService<ILogger<File>>();
+        try {
+            var f = PersistedFileIfExists(def, context);
+            return f == null ? context.WithNew(def) : context.WithUpdated(f, new File(def) { Id = f.Id });
+        }
+        catch (Exception ex) {
+            logger.LogError(ex, $"Error on file: {def.Name}");
+            throw;
+        }
     }
 
-    static File PersistedFileIfExists(File def, IContext context) =>
-       context.Instances<File>().SingleOrDefault(f => f.UniqueRef == def.UniqueRef);
+    private static File PersistedFileIfExists(File def, IContext context) {
+        var logger = context.GetService<ILogger<File>>();
+        var file = context.Instances<File>().SingleOrDefault(f => f.UniqueRef == def.UniqueRef);
+        logger.LogWarning(file is null ? $"Failed to find {def.Name}" : $"Found {def.Name}");
+
+        return file;
+    }
 }
